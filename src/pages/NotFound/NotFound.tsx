7f7f29a3b6ce9426f6ddb81d1a05ba973350c9bc
Ayngie <angelicareutersward@gmail.com>
2025-06-09 15:40:00 +0200

    Style: updated styling for NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,9 +3,13 @@ import './NotFound.scss';
 
 const NotFound: React.FC = () => (
   <main className="not-found">
-    <h1>404 - Page Not Found</h1>
-    <p>Sorry, the page you're looking for doesn't exist.</p>
-    <a href="/">Go back home</a>
+    <h1 className="not-found-title">404 - Page Not Found</h1>
+    <p className="not-found-info">
+      Sorry, the page you're looking for doesn't exist.
+    </p>
+    <a className="not-found-info" href="/">
+      Go back home
+    </a>
   </main>
 );
 
