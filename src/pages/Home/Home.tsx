2c5f0d8e0b2b7a69751636bd8292f80f04a44489
Ayngie <angelicareutersward@gmail.com>
2025-06-09 12:30:11 +0200

    Feat: added font for main titles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './Home.scss';
 
 const Home: React.FC = () => (
-  <section id="Home" className="full-viewport-section">
-    <h1>Front-End Developer</h1>
-    <h2>Portfolio</h2>
+  <section id="Home" className="full-viewport-section home-section">
+    <div className="title-container">
+      <h1 className="home-title FED">Front-End Developer</h1>
+      <h2 className="home-title PORTFOLIO">Portfolio</h2>
+    </div>
     <div className="heart-container">
       <div className="heart" aria-label="Animated heart"></div>
     </div>
