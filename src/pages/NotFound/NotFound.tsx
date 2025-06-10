da50d69a0462e90c15c77d96b784c0e261f4d308
Ayngie <angelicareutersward@gmail.com>
2025-06-10 09:58:26 +0200

    Feat: added accessibility features

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './NotFound.scss';
 
 const NotFound: React.FC = () => (
-  <main className="not-found">
-    <h1 className="not-found-title">404 - Page Not Found</h1>
-    <p className="not-found-info">
+  <section className="not-found" aria-labelledby="notfound-title">
+    <h1 id="notfound-title" className="not-found-title">
+      404 - Page Not Found
+    </h1>
+    <p className="not-found-info" role="alert">
       Sorry, the page you're looking for doesn't exist.
     </p>
-    <a className="not-found-info" href="/">
+    <a className="not-found-info" href="/" aria-label="Go back to the homepage">
       Go back home
     </a>
-  </main>
+  </section>
 );
 
 export default NotFound;
