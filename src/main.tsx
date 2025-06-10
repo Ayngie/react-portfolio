17442e29cad00054eb69edc67d32f4c601a1e562
Ayngie <angelicareutersward@gmail.com>
2025-06-10 10:00:55 +0200

    Refactor: simplified imports and added documentation

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
-import App from './App.tsx';
+import App from './App';
 
+// Entry point: Render the root React component
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
