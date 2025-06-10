81e00ca6d6a73f44477c6410fc18a108f5fe90ed
Ayngie <angelicareutersward@gmail.com>
2025-06-10 16:37:25 +0200

    Feat: added content and external links

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import './Home.scss';
 import Card from '../../components/Card/Card';
 
 const Home: React.FC = () => (
-  <section id="Home" className="home-section">
+  <section id="Home" className="page-section">
     <div className="title-container">
       <h1 className="home-title FED">Front-End Developer</h1>
       <h2 className="home-title PORTFOLIO">Portfolio</h2>
@@ -14,6 +14,30 @@ const Home: React.FC = () => (
     <Card>
       <h3>Hi! I'm Angelica!</h3>
       <p>Welcome to my portfolio.</p>
+      <p> I'm an Osteopath turned Front End Developer. </p>
+      <p>
+        After a decade of working in manual medicine, I discovered a new passion
+        in Front-End Development. As a child of the 80's who grew up with
+        computer games and actual floppy disks, you'd think I would have
+        realized my passion much sooner, but it so happens I also had a passion
+        for health and medicine.
+      </p>
+      <p>
+        Once I realized, I took a Front-End Developer Degree from{' '}
+        <a
+          href="https://medieinstitutet.se/utbildningar/front-end-developer/"
+          target="blank">
+          Medieinstitutet, Stockholm
+        </a>
+        , interned for two periods at{' '}
+        <a
+          href="https://www.postnord.se/"
+          target="_blank"
+          rel="noopener noreferrer">
+          PostNord Group IT
+        </a>
+        , and now I'm here - ready to create amazing web experiences!
+      </p>
     </Card>
   </section>
 );
