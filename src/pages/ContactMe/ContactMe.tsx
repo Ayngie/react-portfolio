fb99d5876f63b9afb6d71029c1eaef7bfae3d48b
Ayngie <angelicareutersward@gmail.com>
2025-06-11 09:41:04 +0200

    Feat: added documentation

--- src/pages/ContactMe/ContactMe.tsx
+++ src/pages/ContactMe/ContactMe.tsx
@@ -9,7 +9,7 @@ type FormData = {
   title: string;
   message: string;
 };
-
+// Keys needed for third-party emailjs.com
 const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID as string;
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID as string;
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY as string;
